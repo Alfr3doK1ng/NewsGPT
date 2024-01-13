9d65ac874fa011ca83020966e9e307c70b302641
Alfred <hlong25@wisc.edu>
2024-01-13 01:55:11 -0600

    Added memory mechanism

--- app.py
+++ app.py
@@ -20,12 +20,23 @@ if __name__ == "__main__":
     vector_store = Pinecone.from_existing_index(index_name, embeddings)
     st.session_state.vs = vector_store
 
-    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature = 1)
-    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 10})
+    llm = ChatOpenAI(model='gpt-4', temperature = 1)
+    retriever = vector_store.as_retriever(search_type='similarity', search_kwargs={'k': 15})
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever = retriever)
+    if 'history' not in st.session_state:
+                st.session_state.history = ''
 
     query = st.text_input('Ask a question about the content of recent news.')
     if query:
-        result = vector_store.similarity_search(query)
-        answer = chain.run(query)
-        st.text_area('LLM Answer: ', value=answer)
+
+        full_query = f"{st.session_state.history}\nUser: {query}"
+
+        # result = vector_store.similarity_search(query)
+        answer = chain.run(full_query)
+        st.text_area('LLM Answer: ', value=answer, height = 200)
+
+        st.divider()
+        
+        value = f'Q: {query}\n A: {answer}'
+        st.session_state.history = f'{value} \n {"-" * 100} \n {st.session_state.history}'
+        st.text_area(label='Chat History', value=st.session_state.history, key='history', height=400)
